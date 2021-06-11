329bbe21e9c395945f3a793787e184f58e4015ee
lucas <lucas@lucas-Aspire-F5-573>
2021-06-11 03:03:17 -0300

    imagem

--- LojaVirtual/src/main/java/br/com/bruce/lojaVirtual/repositorio/EstadoRepositorio.java
+++ LojaVirtual/src/main/java/br/com/bruce/lojaVirtual/repositorio/EstadoRepositorio.java
@@ -1,9 +1,10 @@
 package br.com.bruce.lojaVirtual.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.bruce.lojaVirtual.modelos.Estado;
-
+@Repository
 public interface EstadoRepositorio extends JpaRepository<Estado, Long> {
 
 }
